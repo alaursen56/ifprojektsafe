86ba305bf72dcbd9ca86ee58d7ba4bb3d8479f92
Alexander <76590088+alaursen56@users.noreply.github.com>
2026-04-22 11:18:09 +0200

    Update dict.php

--- dict.php
+++ dict.php
@@ -30,7 +30,7 @@ if ($search !== '') {
       <h1>SAFE</h1>
       <nav>
         <a href="index.php">Forside</a>
-        <a href="test.php">Kodegenerator & test af kodeord</a>
+        <a href="test.php">Test af kodeord</a>
         <a href="dict.php">Ordbog</a>
       </nav>
     </nav>
